b03c784ee5bf1aa4c83f67ee767df51246cf7399
vivnegi21 <vivnegi21@gmail.com>
2023-10-25 16:31:50 +0530

    Connection to DB

--- api/index.js
+++ api/index.js
@@ -1,7 +1,14 @@
 import express from "express";
-
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
 
 
+mongoose.connect("mongodb://127.0.0.1:27017/mern-estate").then(
+    () => console.log("Connected to DB")).catch((err) => {
+        console.log(err);
+    })
+;
 
-app.listen(3000,()=>console.log("Server is running on 3000!"));
+app.listen(3000, () => console.log("Server is running on 3000!"));
