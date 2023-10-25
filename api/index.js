6740e99b21e3ecafb81ed25b5d93a65756666121
vivnegi21 <vivnegi21@gmail.com>
2023-10-25 17:04:52 +0530

    Create test API routes

--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
+import { test } from "./controllers/user.controller.js";
+
 dotenv.config();
 const app = express();
 
@@ -10,5 +13,6 @@ mongoose.connect("mongodb://127.0.0.1:27017/mern-estate").then(
         console.log(err);
     })
 ;
-
+app.get('/',test);
+app.use('/api/user',userRouter);
 app.listen(3000, () => console.log("Server is running on 3000!"));
