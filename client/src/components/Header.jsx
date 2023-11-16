755a8241071853c590d2407ef4e739d297a148a6
vivnegi21 <vivnegi21@gmail.com>
2023-11-16 18:46:10 +0530

    Update header and make profile private

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,10 @@
 import { FaSearch } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
-
+import { useSelector } from 'react-redux';
 
 const Header = () => {
+  const currentUser = useSelector((state) => state.user);
+  console.log(currentUser);
   return (
     <header className='bg-slate-200 shadow-md'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -21,9 +23,20 @@ const Header = () => {
         <ul className='flex gap-4'>
 
           <Link to='/'>
-          <li className='hidden sm:inline text-slate-700 hover:underline'>Home</li></Link>
+            <li className='hidden sm:inline text-slate-700 hover:underline'>Home</li></Link>
           <Link to='/about'><li className='hidden sm:inline text-slate-700 hover:underline'>About</li></Link>
-          <Link to='/sign-in'><li className='text-slate-700 hover:underline'>Sign In</li></Link>
+          <Link to='/profile'>
+            {currentUser.currentUser ? (
+              <img
+                className='rounded-full h-7 w-7 object-cover'
+                src={currentUser.currentUser.photo}
+                alt='profile'
+              />
+            ) : (
+              <li className=' text-slate-700 hover:underline'> Sign in</li>
+            )}
+          </Link>
+
         </ul>
       </div>
     </header>
